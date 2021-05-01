05a5cf0e56de56edeb5cdf98aa1294327de0e04e
Antonio Emilio <antonio_pereira@outlook.com>
2021-05-01 00:54:38 -0300

    Implementando notificações. Falta definir critério de envio (diário, instantâneo...)

--- airpure/src/main/java/br/ufg/airpure/controllers/GeradorDeDados.java
+++ airpure/src/main/java/br/ufg/airpure/controllers/GeradorDeDados.java
@@ -1,5 +1,6 @@
 package br.ufg.airpure.controllers;
 
+import br.ufg.airpure.entity.amostragens;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,9 +9,14 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Random;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import br.ufg.airpure.entity.*;
+import java.time.LocalTime;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -45,10 +51,177 @@ public class GeradorDeDados {
      * E passa para o mecanismo de apuração
      *
      */
-    @Schedule(second = "*/30", minute = "*", hour = "*", info = "Every 1 seconds", persistent = false)
-    public void gera() throws MalformedURLException, IOException, AddressException, MessagingException {
-        System.out.println("Gerando dados para o websocket...");
+    @Schedule(hour = "8", minute = "0", second = "0", persistent = false)
+    public void gera() throws MalformedURLException, IOException, AddressException, MessagingException, InterruptedException {
+        System.out.println("Iniciando verificação para notificações.");
 /*
+        LocalTime now = LocalTime.now();
+        System.out.println(now.getHour());
+        if (!(now.getHour() > 8 && now.getHour() < 17)){
+            System.out.println("Horario nao propicio para envio de alertas.");
+            return;
+        }*/
+
+        Main.db = null;
+        BD.ConectarBD();
+        //String sql = "SELECT DISTINCT ON (id_dispositivos) id_dispositivos,* FROM amostragens WHERE id_dispositivos IN (SELECT id FROM dispositivos WHERE id_projeto = " + idProjetoRelacionado +") ORDER BY id_dispositivos,id DESC;";
+        String sql = "SELECT DISTINCT ON(id_dispositivos) id_dispositivos,* FROM amostragens INNER JOIN usuario ON id_projeto IN (select id_projeto from dispositivos where id = id_dispositivos) ORDER BY id_dispositivos,amostragens.id DESC";
+        String msg = "\n\nVoce está recebendo este aviso porque solicitou um acompanhamento em tempo real.\n\n Foram detectadas alteracoes de parametros fora dos limites pre-definidos.\n\n";
+        String msgTelegram = "Variacao detectada: ";
+        int isNecessary = 0;
+        try {
+            Main.sql = Main.db.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+        ResultSet rs = null;
+        try {
+
+            rs = Main.sql.executeQuery(sql);
+            System.out.println(sql);
+            while (rs.next()) {
+                isNecessary = 0;
+                Users process = new Users();
+                amostragens amostra = new amostragens();
+                process.setChatId(rs.getString("chat_id"));
+                process.setBtoken(rs.getString("btoken"));
+                process.setEmail(rs.getString("email"));
+                process.setNome(rs.getString("nome"));
+                amostra.setCo2(rs.getFloat("co2"));
+                amostra.setEco2(rs.getFloat("eco2"));
+                amostra.setTvoc(rs.getFloat("tvoc"));
+                amostra.setLux(rs.getFloat("lux"));
+                amostra.setDb(rs.getFloat("db"));
+                amostra.setTemperatura(rs.getFloat("temperatura"));
+                amostra.setUmidade(rs.getFloat("umidade"));
+                process.setMaxCO2(rs.getFloat("maxCO2"));
+                process.setMaxUMIDADE(rs.getFloat("maxUMIDADE"));
+                process.setMaxTVOC(rs.getFloat("maxTVOC"));
+                process.setMaxRUIDO(rs.getFloat("maxRUIDO"));
+                process.setMaxLUMINOSIDADE(rs.getFloat("maxLUMINOSIDADE"));
+                process.setMaxTEMPERATURA(rs.getFloat("maxTEMPERATURA"));
+                process.setMaxECO2(rs.getFloat("maxECO2"));
+
+                if (amostra.getEco2() > process.getMaxECO2()) {
+                    msg += "ECO2 - Variacao lida: " + amostra.getEco2() + " ppm | Variacao maxima permitida: " + process.getMaxECO2() + " ppm \n";
+                    msgTelegram += "ECO2: " + amostra.getEco2() + " ppm - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getCo2() > process.getMaxCO2()) {
+                    msg += "CO2 - Variacao lida: " + amostra.getCo2() + " ppm | Variacao maxima permitida: " + process.getMaxCO2() + " ppm \n";
+                     msgTelegram += "CO2: " + amostra.getCo2() + " ppm - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getTvoc() > process.getMaxTVOC()) {
+                    msg += "TVOC - Variacao lida: " + amostra.getTvoc() + " ppb | Variacao maxima permitida: " + process.getMaxTVOC() + " ppb \n";
+                    msgTelegram += "TVOC: " + amostra.getTvoc() + " ppb - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getTemperatura() > process.getMaxTEMPERATURA()) {
+                    msg += "Temperatura - Variacao lida: " + amostra.getTemperatura() + " ºC | Variacao maxima permitida: " + process.getMaxTEMPERATURA() + " ºC \n";
+                    msgTelegram += "Temperatura: " + amostra.getTemperatura()+ " Graus - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getUmidade() > process.getMaxUMIDADE()) {
+                    msg += "Umidade - Variacao lida: " + amostra.getUmidade() + " % | Variacao maxima permitida: " + process.getMaxUMIDADE() + " % \n";
+                    msgTelegram += "Umidade: " + amostra.getUmidade() + " (Relativa) - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getDb() > process.getMaxRUIDO()) {
+                    msg += "Ruído - Variacao lida: " + amostra.getDb() + " dB | Variacao maxima permitida: " + process.getMaxRUIDO() + " dB \n";
+                    msgTelegram += "Ruido: " + amostra.getDb() + " Db - ";
+                    isNecessary = 1;
+                }
+                if (amostra.getLux() > process.getMaxLUMINOSIDADE()) {
+                    msg += "Luminosidade - Variacao lida: " + amostra.getLux() + " lux | Variacao maxima permitida: " + process.getMaxLUMINOSIDADE() + " lux \n";
+                    msgTelegram += "Luminosidade: " + amostra.getLux() + " lux";
+                    isNecessary = 1;
+                }
+
+                if (isNecessary == 1) {
+                    if (process.getEmail() != null) {
+                        Properties props = new Properties();
+                        props.put("mail.smtp.host", "smtp.gmail.com");
+                        props.put("mail.smtp.socketFactory.port", "465");
+                        props.put("mail.smtp.socketFactory.class",
+                                "javax.net.ssl.SSLSocketFactory");
+                        props.put("mail.smtp.auth", "true");
+                        props.put("mail.smtp.port", "465");
+
+                        Session session = Session.getInstance(props,
+                                new javax.mail.Authenticator() {
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication("airpureufg@gmail.com",
+                                        "014589Oi@");
+                            }
+                        });
+                        session.setDebug(true);
+
+                        try {
+
+                            Message message = new MimeMessage(session);
+                            message.setFrom(new InternetAddress("airpureufg@gmail.com"));
+                            //Remetente
+
+                            Address[] toUser = InternetAddress //Destinatário(s)
+                                    .parse(process.getEmail());
+
+                            message.setRecipients(Message.RecipientType.TO, toUser);
+                            message.setSubject("AirPure - Alerta de variação");//Assunto
+                            message.setText(msg);
+                            Transport.send(message);
+
+                        } catch (MessagingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    Thread.sleep(1000);
+
+                    if (process.getBtoken() != null) {
+                        System.out.println("Entrei no telegram.");
+                        String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
+                        String apiToken = process.getBtoken();
+                        String chatId = process.getChatId();
+                        //String apiToken = "1403262308:AAF1zrbdz0-rEyXTdJUdT20MhA0eBpCT_TQ";
+                        //String chatId = "1248387297";
+                        String text = msg;
+
+                        urlString = String.format(urlString, apiToken, chatId, msgTelegram);
+
+                        URL url = new URL(urlString);
+
+                        System.out.println("MINHA URL DE CONEXAO: " + urlString);
+                        URLConnection conn = url.openConnection();
+
+                        StringBuilder sb = new StringBuilder();
+                        InputStream is = new BufferedInputStream(conn.getInputStream());
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        String inputLine = "";
+                        while ((inputLine = br.readLine()) != null) {
+                            sb.append(inputLine);
+                        }
+                        String response = sb.toString();
+                    }
+                    Thread.sleep(1000);
+                }
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        try {
+            Main.db.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(RequestData1.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        /*
         
         SELECT DISTINCT ON(id_dispositivos) id_dispositivos,* FROM amostragens INNER JOIN usuario ON id_projeto IN (select id_projeto from dispositivos where id = id_dispositivos) ORDER BY id_dispositivos,amostragens.id DESC 
         
@@ -106,6 +279,7 @@ public class GeradorDeDados {
             sb.append(inputLine);
         }
         String response = sb.toString();
-*/
+         */
+        System.out.println("Finalizando notificações.");
     }
 }
--- airpure/src/main/java/br/ufg/airpure/entity/Users.java
+++ airpure/src/main/java/br/ufg/airpure/entity/Users.java
@@ -14,6 +14,134 @@ public class Users implements Serializable {
     private String chatId; //Telegram
     private String btoken;  //Telegram
     private projetos projeto;   //Projeto relacionado
+    private float minECO2;
+    private float minCO2;
+    private float minTVOC;
+    private float minUMIDADE;
+    private float minTEMPERATURA;
+    private float minRUIDO;
+    private float minLUMINOSIDADE;
+    private float maxECO2;
+    private float maxCO2;
+    private float maxTVOC;
+    private float maxUMIDADE;
+    private float maxTEMPERATURA;
+    private float maxRUIDO;
+    private float maxLUMINOSIDADE;
+
+    public float getMinECO2() {
+        return minECO2;
+    }
+
+    public void setMinECO2(float minECO2) {
+        this.minECO2 = minECO2;
+    }
+
+    public float getMinCO2() {
+        return minCO2;
+    }
+
+    public void setMinCO2(float minCO2) {
+        this.minCO2 = minCO2;
+    }
+
+    public float getMinTVOC() {
+        return minTVOC;
+    }
+
+    public void setMinTVOC(float minTVOC) {
+        this.minTVOC = minTVOC;
+    }
+
+    public float getMinUMIDADE() {
+        return minUMIDADE;
+    }
+
+    public void setMinUMIDADE(float minUMIDADE) {
+        this.minUMIDADE = minUMIDADE;
+    }
+
+    public float getMinTEMPERATURA() {
+        return minTEMPERATURA;
+    }
+
+    public void setMinTEMPERATURA(float minTEMPERATURA) {
+        this.minTEMPERATURA = minTEMPERATURA;
+    }
+
+    public float getMinRUIDO() {
+        return minRUIDO;
+    }
+
+    public void setMinRUIDO(float minRUIDO) {
+        this.minRUIDO = minRUIDO;
+    }
+
+    public float getMinLUMINOSIDADE() {
+        return minLUMINOSIDADE;
+    }
+
+    public void setMinLUMINOSIDADE(float minLUMINOSIDADE) {
+        this.minLUMINOSIDADE = minLUMINOSIDADE;
+    }
+
+    public float getMaxECO2() {
+        return maxECO2;
+    }
+
+    public void setMaxECO2(float maxECO2) {
+        this.maxECO2 = maxECO2;
+    }
+
+    public float getMaxCO2() {
+        return maxCO2;
+    }
+
+    public void setMaxCO2(float maxCO2) {
+        this.maxCO2 = maxCO2;
+    }
+
+    public float getMaxTVOC() {
+        return maxTVOC;
+    }
+
+    public void setMaxTVOC(float maxTVOC) {
+        this.maxTVOC = maxTVOC;
+    }
+
+    public float getMaxUMIDADE() {
+        return maxUMIDADE;
+    }
+
+    public void setMaxUMIDADE(float maxUMIDADE) {
+        this.maxUMIDADE = maxUMIDADE;
+    }
+
+    public float getMaxTEMPERATURA() {
+        return maxTEMPERATURA;
+    }
+
+    public void setMaxTEMPERATURA(float maxTEMPERATURA) {
+        this.maxTEMPERATURA = maxTEMPERATURA;
+    }
+
+    public float getMaxRUIDO() {
+        return maxRUIDO;
+    }
+
+    public void setMaxRUIDO(float maxRUIDO) {
+        this.maxRUIDO = maxRUIDO;
+    }
+
+    public float getMaxLUMINOSIDADE() {
+        return maxLUMINOSIDADE;
+    }
+
+    public void setMaxLUMINOSIDADE(float maxLUMINOSIDADE) {
+        this.maxLUMINOSIDADE = maxLUMINOSIDADE;
+    }
+    
+    
 
     public projetos getProjeto() {
         return projeto;
